9c477d44814985f261b78d3a74f31b2d9480c206
sandorbelski <108947976+sandorbelski@users.noreply.github.com>
2022-10-10 22:59:27 +0300

    Add files via upload

---
+++ SQL lesson_12_1.sql
@@ -0,0 +1,13 @@
+USE WideWorldImporters
+
+SELECT * FROM Sales.Customers WHERE CustomerName LIKE 'Wingtip Toys%'
+
+SELECT so.OrderID, so.OrderDate, sc.CustomerName, sum(sol.Quantity * sol.UnitPrice)Total FROM Sales.Orders so, Sales.Customers sc, Sales.OrderLines sol WHERE so.CustomerID = sc.CustomerID AND so.OrderID = sol.OrderID GROUP By so.OrderID, so.OrderDate, sc.CustomerName ORDER BY OrderID
+
+SELECT so.OrderID, so.OrderDate, sc.CustomerName, sum(sol.Quantity * sol.UnitPrice)Total FROM Sales.Orders so, Sales.Customers sc, Sales.OrderLines sol WHERE so.CustomerID = sc.CustomerID AND so.OrderID = sol.OrderID AND (so.OrderDate BETWEEN '2016-07-01' AND '2016-07-31') GROUP By so.OrderID, so.OrderDate, sc.CustomerName ORDER BY OrderID
+
+SELECT TOP(1) sc.CustomerName, sum(sol.Quantity * sol.UnitPrice)Total FROM Sales.Orders so, Sales.Customers sc, Sales.OrderLines sol WHERE so.CustomerID = sc.CustomerID AND so.OrderID = sol.OrderID GROUP By sc.CustomerName
+
+SELECT TOP(20) sc.CustomerName, sum(sol.Quantity * sol.UnitPrice)Total FROM Sales.Orders so, Sales.Customers sc, Sales.OrderLines sol WHERE so.CustomerID = sc.CustomerID AND so.OrderID = sol.OrderID GROUP By sc.CustomerName ORDER BY Total DESC
+
+SELECT AVG(Total) FROM (SELECT so.OrderID, so.OrderDate, sc.CustomerName, sum(sol.Quantity * sol.UnitPrice)Total FROM Sales.Orders so, Sales.Customers sc, Sales.OrderLines sol WHERE so.CustomerID = sc.CustomerID AND so.OrderID = sol.OrderID GROUP By so.OrderID, so.OrderDate, sc.CustomerName ) pipec
